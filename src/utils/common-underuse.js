fdc79d9aad286a565747df58a027300ffd11c281
libangquan <q20090255@163.com>
2021-09-02 10:04:43 +0800

    加了几个公用方法

--- src/utils/common-underuse.js
+++ src/utils/common-underuse.js
@@ -18,18 +18,18 @@ export const uniqueArr1 = (arr, keyname) => {
  */
 export const getCountdown = timestamp => {
   // 天数（转换成字符串类型，以防网不好的情况下，出现now）
-  var day = timestamp / (60 * 60 * 24)
-  var days = null
+  let day = timestamp / (60 * 60 * 24)
+  let days = null
   days = day.toString().split('.')[0]
   // 取模（余数）
-  var modulo = timestamp % (60 * 60 * 24)
+  let modulo = timestamp % (60 * 60 * 24)
   // 小时数
-  var hours = Math.floor(modulo / (60 * 60))
+  let hours = Math.floor(modulo / (60 * 60))
   modulo = modulo % (60 * 60)
   // 分钟
-  var minutes = Math.floor(modulo / 60)
+  let minutes = Math.floor(modulo / 60)
   // 秒
-  var seconds = Math.floor(modulo % 60)
+  let seconds = Math.floor(modulo % 60)
   // 输出到页面
   if (days != '0' && days > 0) {
     return days + '天' + hours + '时' + minutes + '分' + seconds + '秒'
@@ -67,7 +67,7 @@ export const popup = (msg = '', time = 2000, type = 'info') => {
  */
 export const engMonthToChiMonth = str => {
   let mm = str.toUpperCase()
-  var em = new Array(
+  let em = new Array(
     'JAN',
     'FEB',
     'MAR',
@@ -136,3 +136,109 @@ export const engMonthToChiMonth = str => {
 //     }
 //   })
 // }
+
+/**
+ * @Description 防抖，一定时间间隔内，多次调用，只跑一次
+ * @param {func} func 要跑的方法
+ * @param {number} wait 间隔时间，默认500ms
+ * @param {boolean} immediate 是否立即执行，默认否
+ */
+export const debounce = (func, wait = 500, immediate = false) => {
+  let timeout
+  return function () {
+    let context = this
+    let args = arguments
+
+    if (timeout) clearTimeout(timeout)
+    if (immediate) {
+      // 如果已经执行过，不再执行
+      let callNow = !timeout
+      timeout = setTimeout(function () {
+        timeout = null
+      }, wait)
+      if (callNow) func.apply(context, args)
+    } else {
+      timeout = setTimeout(function () {
+        func.apply(context, args)
+      }, wait)
+    }
+  }
+}
+
+/**
+ * @Description 节流，多次调用，按一定时间间隔执行
+ * @param {func} func 要跑的方法
+ * @param {number} wait 间隔时间，默认500ms
+ * @param {Object} options
+ */
+export const throttle = (func, wait = 500, options) => {
+  //container.onmousemove = throttle(getUserAction, 1000);
+  let timeout, context, args
+  let previous = 0
+  /**
+   * * options的参数
+   * leading，函数在每个等待时延的开始被调用，默认值为false
+   * trailing，函数在每个等待时延的结束被调用，默认值是true
+   * * 可以根据不同的值来设置不同的效果：
+   * leading-false，trailing-true：默认情况，即在延时结束后才会调用函数
+   * leading-true，trailing-true：在延时开始时就调用，延时结束后也会调用
+   * leading-true, trailing-false：只在延时开始时调用
+   */
+  if (!options) options = { leading: false, trailing: true }
+
+  let later = function () {
+    previous = options.leading === false ? 0 : new Date().getTime()
+    timeout = null
+    func.apply(context, args)
+    if (!timeout) context = args = null
+  }
+
+  let throttled = function () {
+    let now = new Date().getTime()
+    if (!previous && options.leading === false) previous = now
+    let remaining = wait - (now - previous)
+    context = this
+    args = arguments
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout)
+        timeout = null
+      }
+      previous = now
+      func.apply(context, args)
+      if (!timeout) context = args = null
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining)
+    }
+  }
+  return throttled
+}
+
+/**
+ * @Description 对象转化为formdata
+ * @param {object} object
+ */
+export const getFormData = object => {
+  const formData = new FormData()
+  Object.keys(object).forEach(key => {
+    const value = object[key]
+    if (Array.isArray(value)) {
+      value.forEach((subValue, i) => formData.append(key + `[${i}]`, subValue))
+    } else {
+      formData.append(key, object[key])
+    }
+  })
+  return formData
+}
+
+/**
+ * @Description 对象转化为url参数
+ * @param {object} obj
+ */
+export const getParams = obj => {
+  return Object.keys(obj)
+    .map(function (k) {
+      return encodeURIComponent(k) + '=' + encodeURIComponent(obj[k])
+    })
+    .join('&')
+}
